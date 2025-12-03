44cdf7a8ff61cf6eb5a55dc1f84a99514f9bbae4
juraijvu <juraijvuit@gmai.com>
2025-12-03 19:38:44 +0400

    production issue

--- backend/server.js
+++ backend/server.js
@@ -108,6 +108,6 @@ app.use('*', (req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
